e6911d037bdfe65ff07be72984ab4f3f3bfb9c0e
SENA Development Team <dev@sena.edu.co>
2025-12-02 13:53:28 -0500

    Cambios

---
+++ docker-entrypoint.sh
@@ -0,0 +1,15 @@
+﻿#!/bin/bash
+set -e
+
+echo 'Ejecutando migraciones de base de datos...'
+php artisan migrate --force
+
+echo 'Sembrando datos iniciales...'
+php artisan db:seed --force
+
+echo 'Limpiando cache...'
+php artisan cache:clear
+php artisan config:cache
+php artisan route:cache
+
+echo 'Completado!'
