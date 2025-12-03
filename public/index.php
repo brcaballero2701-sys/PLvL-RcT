bbe76935f9565f591de2382457fb0516fe2aa164
brcaballero2701-sys <brcaballero2701@gmail.com>
2025-12-03 16:40:28 -0500

    Update index.php

--- public/index.php
+++ public/index.php
@@ -17,4 +17,15 @@ require __DIR__.'/../vendor/autoload.php';
 /** @var Application $app */
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-$app->handleRequest(Request::capture());
+try {
+    $app->handleRequest(Request::capture());
+} catch (Throwable $e) {
+    // FORZAR que Render Free vea el error real
+    error_log("=== LARAVEL BOOT ERROR ===");
+    error_log($e->__toString());
+
+    http_response_code(500);
+    echo "<pre style='white-space:pre-wrap;font-size:14px'>";
+    echo htmlspecialchars($e->__toString());
+    echo "</pre>";
+}
