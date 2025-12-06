864cbf8a6a2ef02c0e1cfee0e74db4628d94e01c
SENA Development Team <dev@sena.edu.co>
2025-12-05 19:01:02 -0500

    Fix: Usar valores literales en session.php para evitar error de array

--- config/session.php
+++ config/session.php
@@ -1,32 +1,21 @@
 <?php
 
 return [
-
-    'driver' => env('SESSION_DRIVER', 'cookie'),
-
-    'lifetime' => env('SESSION_LIFETIME', 120),
-
+    'driver' => 'cookie',
+    'lifetime' => 120,
     'expire_on_close' => false,
-
     'encrypt' => true,
-
     'files' => storage_path('framework/sessions'),
-
     'connection' => null,
-
     'table' => 'sessions',
-
     'store' => null,
-
     'lottery' => [2, 100],
-
     'cookie' => [
-        'name' => env('SESSION_COOKIE', 'XSRF-TOKEN'),
+        'name' => 'laravel_session',
         'path' => '/',
-        'domain' => env('SESSION_DOMAIN'),
-        'secure' => env('SESSION_SECURE_COOKIES', false),
+        'domain' => null,
+        'secure' => false,
         'http_only' => true,
-        'same_site' => env('SESSION_SAME_SITE', 'lax'),
+        'same_site' => 'lax',
     ],
-
 ];
